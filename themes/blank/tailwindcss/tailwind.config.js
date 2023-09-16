495844a1fe5dfe6c19c6f9650ed038d6adc6b704
Sid Lacy <contact@swlacy.com>
2023-09-15 21:52:55 -0700

    modify color palette

--- themes/blank/tailwindcss/tailwind.config.js
+++ themes/blank/tailwindcss/tailwind.config.js
@@ -1,17 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["../layouts/**/*.html"],
+    content: ["themes/blank/layouts/**/*.html"],
     theme: {
         colors: {
-            "light-primary": "#e6e6e6",
-            "light-secondary": "#f5f5f5",
-            "light-text": "#303030",
+            "light-background": "#f9f9f8",
+            "light-accent": "#ebebe9",
+            "light-text": "#21201c",
 
-            "dark-primary": "#2f353a",
-            "dark-secondary": "#282d34",
-            "dark-text": "#bec5d1",
+            "dark-background": "#1b1b1a",
+            "dark-accent": "#30302e",
+            "dark-text": "#b2b1aa",
 
-            theme: "#6d79bf",
+            theme: "#2d87b4",
             transparent: "transparent",
             current: "currentColor"
         }
